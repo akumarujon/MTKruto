ecec8eebc7ec410526f2aa7333f9e5d4c0e0fbec
Roj <ez@roj.im>
2024-03-08 19:16:05 +0300

    a

--- types/0_message_reference.ts
+++ types/0_message_reference.ts
@@ -1,7 +1,5 @@
 /** Reference to a message. */
 export interface MessageReference {
-  /** Unique identifier of the chat. */
   chatId: number;
-  /** Unique identifier of the message. */
   messageId: number;
 }
--- types/0_price_tag.ts
+++ types/0_price_tag.ts
@@ -1,7 +1,5 @@
 /** A price tag. */
 export interface PriceTag {
-  /** Portion label. */
   label: string;
-  /** Price of the product in the smallest units of the currency (integer, not float/double). */
   amount: number;
 }
